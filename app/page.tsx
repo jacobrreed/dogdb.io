473682dc2d17115357e3234faed267e4fb9b8f78
jacobrreed <jacob.russell.reed@gmail.com>
2024-03-07 13:06:06 -0500

    Main page logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import LogoWithText from "@/app/components/svgs/LogoWithText/LogoWithText";
+
 export default function Home() {
   return (
-    <main className="dark text-foreground bg-background container mx-auto"></main>
+    <main className="dark text-foreground bg-background container flex justify-center">
+      <LogoWithText />
+    </main>
   );
 }
